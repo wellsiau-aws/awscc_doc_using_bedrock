e7faf24cb604195c20eb94e97f72cd6e759561f6
wellsiau-aws <wellsiau@amazon.com>
2024-01-03 14:54:58 -0800

    dependencies

---
+++ install-terraform.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+set -e
+
+echo "Installing Terraform"
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+sudo yum -y install terraform
